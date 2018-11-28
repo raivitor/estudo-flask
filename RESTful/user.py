fd2c3c3a8689d253cc41cee53d6a2e59358ed73a
Rai Vitor <raivitor@gmail.com>
2018-11-28 21:30:09 -0200

    Improved if-else.

--- RESTful/user.py
+++ RESTful/user.py
@@ -17,13 +17,9 @@ class User:
 		query = "SELECT * FROM {table} WHERE username=?".format(table=cls.TABLE_NAME)
 		result = cursor.execute(query, (username,))
 		row = result.fetchone()
-		if row:
-			user = cls(*row)
-		else:
-			user = None
-
+		
 		connection.close()
-		return user
+		return cls(*row) if row else None
 
 	@classmethod
 	def find_by_id(cls, _id):
@@ -33,12 +29,9 @@ class User:
 		query = "SELECT * FROM {table} WHERE id=?".format(table=cls.TABLE_NAME)
 		result = cursor.execute(query, (_id,))
 		row = result.fetchone()
-		if row:
-			user = cls(*row)
-		else:
-			user = None
+		
 		connection.close()
-		return user
+		return cls(*row) if row else None
 
 
 class UserRegister(Resource):
