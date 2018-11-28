f2bca8a8f13c45caa1531a483e8364241fec555d
Rai Vitor <raivitor@gmail.com>
2018-11-28 20:10:28 -0200

    Added parser.

--- RESTful/app.py
+++ RESTful/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
@@ -13,6 +13,12 @@ jwt = JWT(app, authenticate, identity)
 items = []
 
 class Item(Resource):
+	parser = reqparse.RequestParser()
+	parser.add_argument('price',
+		type=float,
+		required=True,
+		help="This field cannot be blank!")
+
 	@jwt_required()
 	def get(self, name):
 		# Usei uma lambda function para verificar se o nome recebido existe.
@@ -25,7 +31,7 @@ class Item(Resource):
 		if next(filter(lambda x: x['name'] == name, items), None):
 			return {'message': "An item with name '{}' already exist.".format(name)}, 400
 
-		data = request.get_json()
+		data = Item.parser.parse_args()
 		item = {'name': name, 'price': data['price']}
 		items.append(item)
 		return item, 201
@@ -36,7 +42,7 @@ class Item(Resource):
 		return {'message': 'Item deleted'}
 
 	def put(self, name):
-		data = request.get_json()
+		data = Item.parser.parse_args()
 		item = next(filter(lambda x: x['name'] == name, items), None)
 		if item is None:
 			item = {'name': name, 'price': data['price']}
