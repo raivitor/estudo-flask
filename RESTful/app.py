5cc900d1b5d1ccfdce27243f7b90c2bf4932587b
Rai Vitor <raivitor@gmail.com>
2018-11-28 19:37:07 -0200

    Added jwt authentication.

--- RESTful/app.py
+++ RESTful/app.py
@@ -1,24 +1,31 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+from flask_jwt import JWT, jwt_required
+
+from security import authenticate, identity
 
 app = Flask(__name__)
+app.secret_key = "raivitor"
 api = Api(app)
+
+jwt = JWT(app, authenticate, identity)
 
 items = []
 
 class Item(Resource):
+	@jwt_required()
 	def get(self, name):
 		# Usei uma lambda function para verificar se o nome recebido existe.
 		# Vai ser retornado um filter object pelo filter, então usei o next para pegar
 		# o primeiro valor e caso não exista nenhum valor, este será None
 		item = next(filter(lambda x: x['name'] == name, items), None)
-		return {'item': None}, 200 if item else 404
+		return {'item': item}, 200 if item else 404
 
 	def post(self, name):
 		if next(filter(lambda x: x['name'] == name, items), None):
 			return {'message': "An item with name '{}' already exist.".format(name)}, 400
 
-		data = requeste.get_json()
+		data = request.get_json()
 		item = {'name': name, 'price': data['price']}
 		items.append(item)
 		return item, 201
--- RESTful/security.py
+++ RESTful/security.py
@@ -2,8 +2,8 @@ from werkzeug.security import safe_str_cmp
 from user import User
 
 users = [
-	User(1, 'Rai Vitor', '123456')
-	User(2, 'Monnaliza Medeiros', '1q2w3e')
+	User(1, 'Rai Vitor', '123456'),
+	User(2, 'Monnaliza Medeiros', '1q2w3e'),
 ]
 
 username_table = {u.username: u for u in users}
@@ -11,7 +11,7 @@ userid_table = {u.id: u for u in users}
 
 def authenticate(username, password):
 	user = username_table.get(username, None)
-	if user and safe_str_cpm(user.password, password):
+	if user and safe_str_cmp(user.password, password):
 		return user
 
 def identity(payload):
