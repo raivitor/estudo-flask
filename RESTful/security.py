e6e4e19a144616e6b628c60264804c20ca3addd0
Rai Vitor <raivitor@gmail.com>
2018-11-28 20:53:10 -0200

    Removed mock users.

--- RESTful/security.py
+++ RESTful/security.py
@@ -1,11 +1,6 @@
 from werkzeug.security import safe_str_cmp
 from user import User
 
-users = [
-	User(1, 'Rai Vitor', '123456'),
-	User(2, 'Monnaliza Medeiros', '1q2w3e'),
-]
-
 username_table = {u.username: u for u in users}
 userid_table = {u.id: u for u in users}
 
