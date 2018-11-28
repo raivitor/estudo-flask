f605d9590b4cf4c1851e658ba8e3244871c03e74
Rai Vitor <raivitor@gmail.com>
2018-11-28 21:17:24 -0200

    Added POST route to create user.

--- RESTful/user.py
+++ RESTful/user.py
@@ -1,4 +1,5 @@
 import sqlite3
+from flask_restful import Resource, reqparse
 
 class User:
 	TABLE_NAME = 'users'
@@ -38,3 +39,37 @@ class User:
 			user = None
 		connection.close()
 		return user
+
+
+class UserRegister(Resource):
+	TABLE_NAME = 'users'
+
+	parser = reqparse.RequestParser()
+	parser.add_argument(
+		'username',
+		type=str,
+		required=True,
+		help="This field cannot be blank!")
+
+	parser.add_argument(
+		'password',
+		type=str,
+		required=True,
+		help="This field cannot be blank!")
+
+	def post(self):
+		data = UserRegister.parser.parse_args()
+
+		if User.find_by_username(data['username']):
+			return {"message": "User with that username already exists."}, 400
+
+		connection = sqlite3.connect('data.db')
+		cursor = connection.cursor()
+
+		query = "INSERT INTO {table} VALUES (NULL, ?, ?)".format(table=self.TABLE_NAME)
+		cursor.execute(query, (data['username'], data['password']))
+
+		connection.commit()
+		connection.close()
+
+		return {"message": "User created successfully."}, 201
