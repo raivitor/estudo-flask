d0ed9f3beb2895784a19a57b041a17a1e5584bcd
Rai Vitor <raivitor@gmail.com>
2018-11-29 18:10:23 -0200

    Added POST Item route and few refactorings.

--- RESTful/item.py
+++ RESTful/item.py
@@ -12,26 +12,25 @@ class Item(Resource):
 
 	@jwt_required()
 	def get(self, name):
-		connection = sqlite3.connect('data.db')
-		cursor = connection.cursor()
-
-		query = "SELECT * FROM items WHERE name=?"
-		result = cursor.execute(query, (name,))
-		row = result.fetchone()
-		connection.close()
-
-		if row:
-			return {'item': {'name': row[0], 'price': row[1]}}
+		item = self.find_by_name(name)
+		if item:
+				return item
 		return {'message': 'Item not found'}, 404
 
 	def post(self, name):
-		if next(filter(lambda x: x['name'] == name, items), None):
-			return {'message': "An item with name '{}' already exist.".format(name)}, 400
+		if self.find_by_name(name):
+			return {'message': "An item with name '{}' already exists.".format(name)}
 
 		data = Item.parser.parse_args()
+
 		item = {'name': name, 'price': data['price']}
-		items.append(item)
-		return item, 201
+
+		try:
+			Item.insert(item)
+		except:
+			return {"message": "An error occurred inserting the item."}
+
+		return item
 
 	def delete(self, name):
 		global items
@@ -47,6 +46,30 @@ class Item(Resource):
 		else:
 			item.update(data)
 		return item
+
+	@classmethod
+	def find_by_name(cls, name):
+		connection = sqlite3.connect('data.db')
+		cursor = connection.cursor()
+
+		query = "SELECT * FROM {table} WHERE name=?".format(table=cls.TABLE_NAME)
+		result = cursor.execute(query, (name,))
+		row = result.fetchone()
+		connection.close()
+
+		if row:
+			return {'item': {'name': row[0], 'price': row[1]}}
+
+	@classmethod
+	def insert(cls, item):
+		connection = sqlite3.connect('data.db')
+		cursor = connection.cursor()
+
+		query = "INSERT INTO {table} VALUES(?, ?)".format(table=cls.TABLE_NAME)
+		cursor.execute(query, (item['name'], item['price']))
+
+		connection.commit()
+		connection.close()
 
 
 class ItemList(Resource):
