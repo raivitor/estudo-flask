341ec440ebc4f85fe516ed35b4eb872c51a8ba3e
Rai Vitor <raivitor@gmail.com>
2018-11-29 23:32:58 -0200

    Added SQLAlchemy database.

--- app.py
+++ app.py
@@ -7,8 +7,15 @@ from resources.user import UserRegister
 from resources.item import Item, ItemList
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = "raivitor"
 api = Api(app)
+
+@app.before_first_request
+def create_tables():
+  db.create_all()
 
 jwt = JWT(app, authenticate, identity)
 
@@ -16,4 +23,7 @@ api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(UserRegister, '/register')
 
-app.run(port=5000)
+if __name__ == '__main__':
+  from db import db
+  db.init_app(app)
+  app.run(port=5000, debug=True)
--- models/user.py
+++ models/user.py
@@ -1,8 +1,14 @@
 import sqlite3
+from db import db
 
-class User:
+class User(db.Model):
 	TABLE_NAME = 'users'
+	__tablename__ = 'users'
 
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(80))
+	password = db.Column(db.String(80))
+	
 	def __init__(self, _id, username, password):
 		self.id = _id
 		self.username = username
